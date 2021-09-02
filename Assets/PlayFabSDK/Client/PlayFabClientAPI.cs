c675a512be90aab37428e1ceb1b7437c55a3ae6f
Bogdanum <56800900+Bogdanum@users.noreply.github.com>
2021-09-02 21:32:24 +0300

    M1

--- Assets/PlayFabSDK/Client/PlayFabClientAPI.cs
+++ Assets/PlayFabSDK/Client/PlayFabClientAPI.cs
@@ -105,11 +105,6 @@ namespace PlayFab
             PlayFabHttp.MakeApiCall("/Client/AddSharedGroupMembers", request, AuthType.LoginSession, resultCallback, errorCallback, customData, extraHeaders, context, callSettings);
         }
 
-        public static object GetPlayerProfile(GetPlayerProfileRequest getPlayerProfileRequest)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
         /// Adds playfab username/password auth to an existing account created via an anonymous auth method, e.g. automatic device
         /// ID login.
